b26eb10ec9ebdd59506e9f3c2ab5ff74ac13a81b
Oleksandr Kokhno <kohnoan@gmail.com>
2023-05-30 01:31:23 +0300

    commit1

--- src/shared/services/api.js
+++ src/shared/services/api.js
@@ -1,68 +1,68 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const instance = axios.create({
-//   baseURL: 'https://api.themoviedb.org/3',
-//   params: {
-//     api_key: '432ccaf7113b52e993b924f1fb358945',
-//     language: 'en-US',
-//   },
-// });
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: '432ccaf7113b52e993b924f1fb358945',
+    language: 'en-US',
+  },
+});
 
-// export async function getTrendingMovies(page) {
-//   const query = `/trending/movie/week`;
-//   try {
-//     const { data } = await instance.get(query, {
-//       params: {
-//         page,
-//       },
-//     });
+export async function getTrendingMovies(page) {
+  const query = `/trending/movie/week`;
+  try {
+    const { data } = await instance.get(query, {
+      params: {
+        page,
+      },
+    });
 
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
 
-// export async function getMovieDetails(id) {
-//   const query = `/movie/${id}`;
-//   try {
-//     const { data } = await instance.get(query);
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+export async function getMovieDetails(id) {
+  const query = `/movie/${id}`;
+  try {
+    const { data } = await instance.get(query);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
 
-// export async function getMovieCast(id) {
-//   const query = `/movie/${id}/credits`;
-//   try {
-//     const { data } = await instance.get(query);
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+export async function getMovieCast(id) {
+  const query = `/movie/${id}/credits`;
+  try {
+    const { data } = await instance.get(query);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
 
-// export async function getMovieReviews(id) {
-//   const query = `/movie/${id}/reviews`;
-//   try {
-//     const { data } = await instance.get(query);
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+export async function getMovieReviews(id) {
+  const query = `/movie/${id}/reviews`;
+  try {
+    const { data } = await instance.get(query);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
 
-// export async function searchMovie(queryString, page) {
-//   const query = `/search/movie?query=${queryString}`;
-//   try {
-//     const data = await instance.get(query, {
-//       params: {
-//         page,
-//       },
-//     });
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
+export async function searchMovie(queryString, page) {
+  const query = `/search/movie?query=${queryString}`;
+  try {
+    const data = await instance.get(query, {
+      params: {
+        page,
+      },
+    });
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
